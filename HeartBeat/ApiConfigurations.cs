3bbd7260983f80a2ae0bc33ce805309154ffe411
Kenneth John Roxas <kenneth@cloudswyft.com>
2023-05-02 15:00:30 +0800

    First Commit

---
+++ HeartBeat/ApiConfigurations.cs
@@ -0,0 +1,144 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeartBeat
+{
+    public class ApiConfigurations
+    {
+        public static string url = "https://development-api.cloudswyft.com/labs/virtualmachine/";
+        public static string tenantId = "a7db876451fc4e2387bebda3856bcf7f";
+        public static string subscriptionId = "44e7f1ad-c881-4c6c-9af1-86d478cb5f74";
+        public static string clmpURL = "https://labs-staging.cloudswyft.com/scl/api/";
+
+        public async Task<string> ShutdownVM(string resourceId)
+        {
+            string response = null;
+
+            try
+            {
+                var jsonVMData = new { resourceId = resourceId, operation = "Stop" };
+
+                var data = JsonConvert.SerializeObject(jsonVMData);
+
+                response = await ApiVMCall("PUT", tenantId, subscriptionId, url, data);
+
+                return response;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error   --- " + e.InnerException.Message);
+            }
+            return response;
+
+        }
+
+
+        public async Task<string> UpdateConsumedHours(string url, string data)
+        {
+            string response = null;
+
+            try
+            {
+                response = await ApiCall("PUT", url, data);
+                
+                return "OK";
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+        }
+
+     
+        private async Task<string> ApiVMCall(string method, string tenantId, string subscriptionId, string url, string serializeData = null)
+        {
+            HttpResponseMessage response = null;
+
+            try
+            {
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+                HttpClient client = new HttpClient();
+                client.BaseAddress = new Uri(url);
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                client.DefaultRequestHeaders.Add("TenantId", tenantId);
+                client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", subscriptionId);
+
+                //var jsonVMData = new { resourceId = resourceId, operation = "Stop" };
+
+                //data = JsonConvert.SerializeObject(jsonVMData);
+                
+                if (method == "POST")
+                {
+                    response = await client.PostAsync(client.BaseAddress + url, new StringContent(serializeData));
+                }
+                else if (method == "GET")
+                {
+                    response = await client.GetAsync(client.BaseAddress + url).ConfigureAwait(false);
+                }
+                else if (method == "PUT")
+                {
+                    response = await client.PutAsync(client.BaseAddress, new StringContent(serializeData, Encoding.UTF8, "application/json"));
+                }
+
+                return await response.Content.ReadAsStringAsync();
+            }
+            catch (Exception e)
+            {
+                return e.InnerException.Message;
+            }
+        }
+
+        private async Task<string> ApiCall(string method, string url, string serializeData = null)
+        {
+            HttpResponseMessage response = null;
+
+            try
+            {
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+                HttpClient client = new HttpClient();
+                client.BaseAddress = new Uri(url);
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+             
+
+                //var jsonVMData = new { resourceId = resourceId, operation = "Stop" };
+
+                //data = JsonConvert.SerializeObject(jsonVMData);
+
+                if (method == "POST")
+                {
+                    response = await client.PostAsync(client.BaseAddress + url, new StringContent(serializeData));
+                }
+                else if (method == "GET")
+                {
+                    response = await client.GetAsync(client.BaseAddress + url).ConfigureAwait(false);
+                }
+                else if (method == "PUT")
+                {
+                    response = await client.PutAsync(client.BaseAddress, new StringContent(serializeData, Encoding.UTF8, "application/json"));
+                }
+
+                return await response.Content.ReadAsStringAsync();
+            }
+            catch (Exception e)
+            {
+                return e.InnerException.Message;
+            }
+        }
+        //class JsonVM
+        //{
+        //    public static string ResourceId = resourceId;
+        //    public static string Operation = "Stop";
+        //}
+
+    }
+}
---
+++ HeartBeat/MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Globalization;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace HeartBeat
+{
+    public partial class MainWindow : Window
+    {
+        private DispatcherTimer timer;
+        public AllInputSources lastInput;
+        private KeyboardInput keyboard;
+        private MouseInput mouse;
+
+        private DateTime mouseTimer;
+        private DateTime keyboardTimer;
+
+        public static string resourceId = "edcdb4c9-6b44-438e-b983-858850a00e87";
+        public string tenantKey;
+        public string subscriptionKey;
+        public string labsAPIURL;
+        public string computerName;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+           // Application.Current.MainWindow.Visibility = Visibility.Hidden;
+            keyboard = new KeyboardInput();
+            keyboard.KeyBoardKeyPressed += keyboard_KeyBoardKeyPressed;
+
+            mouse = new MouseInput();
+            mouse.MouseMoved += mouse_MouseMoved;
+            lastInput = new AllInputSources();
+            timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan();
+            timer.Interval = new TimeSpan(0, 0, 0, 15); //check every 15 seconds
+            timer.Tick += timer_Tick;
+            timer.Start();
+        }
+
+        void keyboard_KeyBoardKeyPressed(object sender, EventArgs e)
+        {
+            keyboardTime.Content = DateTime.Now;
+            keyboardTimer = DateTime.Now;
+            //keyboardTime.Content = FormatDateTime(DateTime.Now);
+        }
+
+        private string FormatDateTime(DateTime dateTime)
+        {
+            //return dateTime.ToString("HH:mm", CultureInfo.CurrentUICulture);
+            return dateTime.ToString("HH:mm:ss fff", CultureInfo.CurrentUICulture);
+        }
+        void mouse_MouseMoved(object sender, EventArgs e)
+        {
+            mouseTime.Content = DateTime.Now;
+            mouseTimer = DateTime.Now;
+            //mouseTime.Content = FormatDateTime(DateTime.Now);
+        }
+        
+        async void timer_Tick(object sender, EventArgs e)
+        {
+            ApiConfigurations apiCall = new ApiConfigurations();
+
+            Console.WriteLine(DateTime.Now);
+            TimeSpan idleMouse = DateTime.Now - mouseTimer; //Difference between active MOUSE vs. not active MOUSE
+            TimeSpan idleKeyboard = DateTime.Now - keyboardTimer; //Difference between active KEYBOARD vs. not active KEYBOARD
+
+            lastInputTime.Content = FormatDateTime(lastInput.GetLastInputTime());
+            Console.WriteLine("Idle Minutes --" + idleMouse.Minutes);
+
+            //check if idle in 10 minutes then show dialog box
+            //if(idleMouse.Minutes >= 1 && idleKeyboard.Minutes >= 1)
+            //{
+            //    Application.Current.MainWindow.Visibility = Visibility.Visible;
+            //}
+
+            if (mouseTimer != null || keyboardTimer != null) // user is active
+            {
+                //check if idle in 15 minutes then autoshutdown trigger
+                if (idleMouse.Minutes >= 1 && idleKeyboard.Minutes >= 1)
+                {
+                    var jsonVMData = new { computerName =  computerName};
+                    var data = JsonConvert.SerializeObject(jsonVMData);
+
+                    await apiCall.UpdateConsumedHours(labsAPIURL + "/MachineLabs/UpdateConsumedHours" , data);
+                    await apiCall.ShutdownVM(resourceId);
+
+                    Console.WriteLine("Mouse : " + mouseTime.Content + " AND " + "Keyboard : " + keyboardTime.Content);
+                }
+            }
+            else
+            {
+                mouseTime.Content = DateTime.Now;
+                keyboardTime.Content = DateTime.Now;
+            }
+        }
+
+    }
+}
